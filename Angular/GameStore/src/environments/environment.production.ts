0d4726198839c76a14769606ec1e33b4194b8f89
s.grazhdankin <s.grazhdankin91@gmail.com>
2025-04-21 23:13:58 +0700

    deploy angular to k8s

---
+++ Angular/GameStore/src/environments/environment.production.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    authUrl: 'https://auth-clusterip-service:8080/api/auth',
+    productUrl: 'https://product-clusterip-service:8080/api/products',
+    userUrl: 'https://user-clusterip-service:8080/api/users',
+    cartUrl: 'https://cart-clusterip-service:8080/api/carts'
+};
